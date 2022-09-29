fa8ff4bc2ceb1e99697cf3928ba001b8aa7d2aaf
zaljifri <zainab.aljifri@gmail.com>
2022-09-29 20:18:07 +0300

    Fix responsivity

--- src/Items.jsx
+++ src/Items.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 
-
 function Items(props) {
     return (
-        <div className="item">
+        <div className="col col-lg-3 item">
             <div className="item-title">
                 {props.name}
             </div>
             <div className="item-description">
-                {props.description}
+                <p>{props.description}</p>
                 <img src={props.url} alt=""></img>
             </div>
         </div>
